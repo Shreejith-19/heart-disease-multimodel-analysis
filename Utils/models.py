7d4e87c6819a88d3e46e1141765d6fc68057e128
Shreejith-19 <shreejith1919@gmail.com>
2025-08-14 20:20:22 +0530

    update test_model to be more informative

--- Utils/models.py
+++ Utils/models.py
@@ -1,6 +1,6 @@
 
 from sklearn.model_selection import GridSearchCV
-from sklearn.metrics import classification_report, accuracy_score
+from sklearn.metrics import classification_report, accuracy_score, f1_score
 class Models:
     def train_random_forest_model(self,x_train, y_train, rdf_model, params_grid):
         """
@@ -22,9 +22,14 @@ class Models:
     def test_model(self,trained_model, x_train, y_train, x_test, y_test):
         """
         """
-        print("Training Results: ")
-        print(classification_report(y_train, trained_model.predict(x_train)))
-        print("Test Results :")
-        print(classification_report(y_test, trained_model.predict(x_test)))
-        print("test_accuracy = ",accuracy_score(trained_model.predict(x_test), y_test))
-
+        y_pred_train = trained_model.predict(x_train)
+        print("Training Results:")
+        print(f"accuracy score = {accuracy_score(y_pred_train, y_train)}")
+        print(f"Macro-F1 = {f1_score(y_pred_train, y_train, average = "macro")}")
+        print(f"Weighted-F1 = {f1_score(y_pred_train, y_train, average = "weighted")}")
+        print("\n")
+        y_pred_test = trained_model.predict(x_test)
+        print("Testing Results:")
+        print(f"accuracy score = {accuracy_score(y_pred_test, y_test)}")
+        print(f"Macro-F1 = {f1_score(y_pred_test, y_test, average = "macro")}")
+        print(f"Weighted-F1 = {f1_score(y_pred_test, y_test, average = "weighted")}")
